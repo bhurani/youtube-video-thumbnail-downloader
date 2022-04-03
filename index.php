217c7b87838fcf4eb7fb481de77f7cf99cfe5262
Mayank Bhurani <102176282+bhurani@users.noreply.github.com>
2022-04-03 12:07:02 +0530

    Meta keywords

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="">
-  <meta name="keywords" content="HTML, CSS, JavaScript">
+  <meta name="keywords" content="HTML, CSS, JavaScript, PHP, cURL">
   <meta name="author" content="Mayank Bhurani">
   <link rel="icon" type="image/x-icon" sizes="32x32" href="./img/favicon.ico">
   <title>Bhurani | Youtube Video Thumbnail Downloader</title>
